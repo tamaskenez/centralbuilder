a5b131e69fe846df8f954656dc7b30ab29b5787c
Tamas Kenez <tamas.kenez@adasworks.com>
2015-10-01 16:59:23 +0200

    remove packages.cmake (was added accidentally)

--- packages.cmake
+++
@@ -1,5 +0,0 @@
-set(SCM https://scm.adasworks.com/r)
-set(TPS ${SCM}/thirdparty/src)
-
-add_pkg(ZLIB GIT_REPOSITORY ${TPS}/zlib CMAKE_ARGS "-DVARIABLE=contains space")
-add_pkg(PNG GIT_REPOSITORY ${TPS}/png DEPENDS ZLIB)
